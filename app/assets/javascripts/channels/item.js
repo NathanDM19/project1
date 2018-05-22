ca3c2bc30e31455700f711327b59e6d9b2d1184a
Nathan Del Medico <creationmatee@gmail.com>
2018-05-22 13:52:43 +1000

    Fixed walls, added enemy, changed player sprite, healthbars and nameplates

--- app/assets/javascripts/channels/item.js
+++ app/assets/javascripts/channels/item.js
@@ -5,7 +5,16 @@ App.item = App.cable.subscriptions.create('ItemChannel', {
   received: function(data) {
     testVar = data['gold'];
     testVar2 = data['currentCharacter']
-    playerDetails[ data['currentCharacter'] ] = {'xPos': data['xPos'], 'yPos': data['yPos'], 'direction': data['direction'], name: data['name'], health: data['health']}
+    playerDetails[ data['currentCharacter'] ] = {'xPos': data['xPos'], 'yPos': data['yPos'], 'direction': data['direction'], name: data['name'], health: data['health'], enemy: data['enemy']}
+    enemyHealth = data['enemyHealth'];
+    if (enemyHealth <= 0) {
+      // enemyHealth = 100;
+      singleEnemy = true;
+      enemy.disableBody(true, true)
+      enemyHealthBarBack.setScale(0)
+      window.setTimeout(createEnemy, 1000)
+    }
+
     /*
     // if (playerSprites[`player${data['currentCharacter']}`].x !== playerDetails[data['currentCharacter']]['xPos'] || playerSprites[`player${data['currentCharacter']}`].y !== playerDetails[data['currentCharacter']]['yPos']) {
     //   playerSprites[`player${data['currentCharacter']}`].x = playerDetails[data['currentCharacter']]['xPos']
@@ -15,7 +24,7 @@ App.item = App.cable.subscriptions.create('ItemChannel', {
     // console.log(playerPositions)
     */
   },
-  create: function(gold, currentChar, xPos, yPos, direction, health){
-    this.perform('create', {gold, currentCharacter, xPos, yPos, direction, health});
+  create: function(gold, currentChar, xPos, yPos, direction, health, enemy, enemyHealth){
+    this.perform('create', {gold, currentCharacter, xPos, yPos, direction, health, enemy, enemyHealth});
   }
 });
